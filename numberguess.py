458f81a675ea17d91905e7d4b46c86c149ae4a9a
akanksha negi <182057372+akankshauw@users.noreply.github.com>
2025-03-02 11:43:38 +0530

    minor updates bec i have an exam tomorrow

--- numberguess.py
+++ numberguess.py
@@ -1,5 +1,5 @@
 import random
-from colorama import Fore, Style
+from colorama import Fore, Style #for cool colours in boring terminal
 
 def numberguess():
     print(Fore.CYAN + "Welcome to the number guessing game ! Let's begin, shall we?" + Style.RESET_ALL)
@@ -13,7 +13,7 @@ def numberguess():
             break
 
         elif choice.lower() != 'y':
-            print(Fore.LIGHTMAGENTA_EX + "Please choose from either 'y' or 'n' :>" + Style.RESET_ALL)
+            print(Fore.LIGHTMAGENTA_EX + "Please choose from either 'y' or 'n' :>" + Style.RESET_ALL) #if something else than 'y' or 'n'
 
         else:
             random_number = random.randint(1,100)
@@ -45,13 +45,13 @@ def numberguess():
                         print(Fore.RED + "oof, too low! try again?" + Style.RESET_ALL)
                         count += 1
 
-                except ValueError:
+                except ValueError: #incase the user enters something other than an integer 
                     print(Fore.LIGHTRED_EX + "Please enter an integer only!" + Style.RESET_ALL)
 
             else:
                 print(Fore.MAGENTA + f"Sorry, you've run out of attempts :/ the number was {random_number}. No worries, we'll get the next one!" + Style.RESET_ALL)
 
-            print(Fore.LIGHTMAGENTA_EX + "Game Over. Would you play again?" + Style.RESET_ALL)
+            print(Fore.LIGHTMAGENTA_EX + "Game Over. Would you like to play again?" + Style.RESET_ALL)
 
 if __name__ == "__main__":
     numberguess()
